c85e5cbcb25146e341fdbb4be6b980090b5908cc
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2024-05-30 17:22:57 -0300

    att repo

--- SEGUNDO BIMESTRE/aed_aula28-05/src/Classes/ProductsService.java
+++ SEGUNDO BIMESTRE/aed_aula28-05/src/Classes/ProductsService.java
@@ -12,57 +12,63 @@ public class ProductsService {
         .forEach(produto -> System.out.println("-> "+ produto.nome));//Printa os produtos
     }
 
+    //Utiliza do Stream para realizar a soma de todos os preços dos itens.
     public void somaPrecos(List<Produto> lista){
         double soma = lista.stream()
-        .mapToDouble(produto -> produto.preco)
-        .sum();
+        .mapToDouble(produto -> produto.preco) //Mapeando os valores do tipo Double da Lista
+        .sum();//Realizando a soma dos mesmos
 
-        System.out.println(soma);
+        System.out.println("Total de soma: R$"+soma);//Printando resultado
     }
 
+    //Utiliza o stream para verificar qual é item mais caro 
     public void produtoMaisCaro(List<Produto> lista){
-        Produto produtoComMaiorValor =  lista.stream()
-         .max(Comparator.comparingDouble((p1)-> p1.preco))
-         .get();
-         System.out.println(produtoComMaiorValor.nome);
+        Produto produtoComMaiorValor =  lista.stream() 
+         .max(Comparator.comparingDouble((p1)-> p1.preco))//Realiza a comparação dos doubles, substituindo a posição naquele que for maior
+         .get();//Obtem o dado da lista
+         System.out.println("O produto mais caro da lista é: "+produtoComMaiorValor.nome);
          
     }
 
+// Utiliza do stream para odernar os produtos em ordem alfabética
     public void produtosOrdenados(List<Produto> lista){
         lista.stream()
-        .sorted((o1, o2) -> o1.nome.compareTo(o2.nome))
-        .forEach(produto -> System.out.println(produto.nome));
+        .sorted((o1, o2) -> o1.nome.compareTo(o2.nome))//Compara os nomes dos produtos
+        .forEach(produto -> System.out.println(produto.nome));//A cada item da lsita ele printa 
     }
-
+//Realiza a aplicação de desconto de 10% em cada item da lista 
     public void aplicarDesconto(List<Produto> lista){
         lista.stream()
         .forEach(produto -> {
-            produto.aplicarDesconto();
-            System.out.println(produto.nome+ "Valor: "+ produto.preco);
+            produto.aplicarDesconto();//Chama a função aplicar desconto para cada item da lista; 
+            System.out.println(produto.nome+ "Valor: "+ produto.preco);//Printa os produtos com o seus novos valores
         });
         
     }
 
+    //Printa os produtos que estão na faixa de preço de 30 a 100;
     public void contarProdutosComPrecoNaFaixa(List<Produto> lista){
         double quanitdadeProdutos = lista.stream()
-        .filter(produto -> produto.preco >= 30.0 && produto.preco <= 100.0)
-        .count();
+        .filter(produto -> produto.preco >= 30.0 && produto.preco <= 100.0)//Realiza essa filtragem
+        .count();//Realiza a contagem 
 
-        System.out.println(quanitdadeProdutos);
+        System.out.println(quanitdadeProdutos);//Printa os produtos
     }
 
+    //Printa os produtos de acorodo com o valor 50, se é maior ou menor
     public void contarProdutosDeAcordoValor(List<Produto> lista){
-        lista.stream()
-        .filter(produto -> produto.preco <= 50)
-        .toList()
-        .forEach(System.out::println);
+        double abaixo = lista.stream()
+        .filter(produto -> produto.preco < 50)//Realiza o filtro
+        .count();
+        System.out.println("Produtos abaixo ou igual a R$50,00: "+abaixo);//Itera essa lista
 
-        lista.stream()
+        double acima = lista.stream()
         .filter(produto -> produto.preco >= 50)
-        .toList()
-        .forEach(System.out::println);
+        .count();
+        System.out.println("Produtos acima de R$50,00: "+acima);
     }
 
+    //COncatena os produtos, um ao lado do outro
     public void concatenarProdutos(List<Produto> lista){
        String listaJunta = lista.stream()
         .map(produto -> produto.nome)
