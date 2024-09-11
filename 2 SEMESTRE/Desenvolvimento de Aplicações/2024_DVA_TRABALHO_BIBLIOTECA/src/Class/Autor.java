7495973d331ab64defd3d7b302ced851daec92fb
DevsMarcos <128372293+DevsMarcos@users.noreply.github.com>
2024-09-11 09:21:13 -0300

    Update Autor.java
    Adição de comentários

--- 2 SEMESTRE/Desenvolvimento de Aplicações/2024_DVA_TRABALHO_BIBLIOTECA/src/Class/Autor.java
+++ 2 SEMESTRE/Desenvolvimento de Aplicações/2024_DVA_TRABALHO_BIBLIOTECA/src/Class/Autor.java
@@ -5,29 +5,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+//Parâmetros da Classe
 public class Autor {
     private final String nome;
     private final int anoDenascimento;
     private final String nacionalidade;
     private List<Livro> livros;
 
-
+    //Construtor da Classe Autor. inicia uma nova Lista para cada autor instânciado
     public Autor(String nome, String nacionalidade, int anoDenascimento) {
         this.nome = nome;
         this.livros = new ArrayList<>();
         this.nacionalidade = nacionalidade;
         this.anoDenascimento = anoDenascimento;
     }
-
+    
+//Função para retornar o nome do Autor 
     public String getNome() {
         return nome;
     }
 
+    //Função com nível de acesso Package-Private. Sendo assim é possível visualizar a mesma
+    //Apenas dentro do pacote onde está. Tem por objetivo adicionar um livro à lista de livros do Autor
     void adicionarLivro(Livro livro){
 
         livros.add(livro);
     }
 
+    //Função que retorna os livros de um autor. É protegida pelo métodou nmodifiableList.
+    //Serve apenas para visualização
     public List<Livro> getLivros(){
 
         return Collections.unmodifiableList(livros);
