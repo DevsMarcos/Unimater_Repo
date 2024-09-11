4d4afcad3492636a290d983844eb1de25c9ba77c
DevsMarcos <128372293+DevsMarcos@users.noreply.github.com>
2024-09-11 13:12:39 -0300

    Update Membro.java

--- 2 SEMESTRE/Desenvolvimento de Aplicações/2024_DVA_TRABALHO_BIBLIOTECA/src/Class/Membro.java
+++ 2 SEMESTRE/Desenvolvimento de Aplicações/2024_DVA_TRABALHO_BIBLIOTECA/src/Class/Membro.java
@@ -4,31 +4,36 @@ package Class;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+//Atributos da Classe Membro
 public class Membro {
     private String nome;
     private int idMembro;
     private List<Livro> historicoDeLivros;
 
+    //Construtor da Classe membro, recebe como parâmetros os atributos nome e idMembro. A mesma realiza a criação
+    //De uma nova lista referente ao histórico de livros do membro instanciado. 
     public Membro(String nome, int idMembro) {
         this.nome = nome;
         this.idMembro = idMembro;
         this.historicoDeLivros = new ArrayList<>();
     }
 
+    //Método que retorna a lista de livros, no caso, o histórico de livros emprestados pelo membro.
     public List<Livro> historicoDeLivros(){
         return Collections.unmodifiableList(historicoDeLivros);
     }
 
-
+//Método que retorna o nome do membro
     public String getNome() {
         return nome;
     }
-
+//Método que retorna o Id do membro;
     public int getIdMembro() {
         return idMembro;
     }
 
+    //Método que realiza a adição de novos livros ao histórico do memobr. Possui o nível de acesso definido
+    //como Private-Package. 
     void adicionarLivro(Livro livro){
         historicoDeLivros.add(livro);
     }
