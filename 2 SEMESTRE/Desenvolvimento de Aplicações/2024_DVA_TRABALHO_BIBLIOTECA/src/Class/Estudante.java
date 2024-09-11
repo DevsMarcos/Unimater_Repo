8b2cda5554a4926fa481f0c3deff1cab695e11f1
DevsMarcos <128372293+DevsMarcos@users.noreply.github.com>
2024-09-11 09:32:09 -0300

    Update Estudante.java
    Adição de Comentários

--- 2 SEMESTRE/Desenvolvimento de Aplicações/2024_DVA_TRABALHO_BIBLIOTECA/src/Class/Estudante.java
+++ 2 SEMESTRE/Desenvolvimento de Aplicações/2024_DVA_TRABALHO_BIBLIOTECA/src/Class/Estudante.java
@@ -1,14 +1,18 @@
 package Class;
 
-
+//Classe estudante que herda a Classe Membro
 public class Estudante extends Membro {
+    //Possui como atributo adicional StringCurso; 
     private String curso;
 
+    //Método Construtor, recebe como parâmetro os atributos da Classe Membro mais o atributo específico da Classe Estudante.
     public Estudante(String nome, int idMembro, String curso) {
         super(nome, idMembro);
         this.curso = curso;
     }
 
+    //Métodos toString(), retorna uma representação em forma de String(tetxo), do objeto.
+    //Aqui refereênciamos os atributos da classe Membro e da Classe Estudante, para obeter os valores.
     @Override
     public String toString() {
         return "\n"+
