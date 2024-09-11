8c1b2122eab39761689eb705ccc94d48daba2f99
DevsMarcos <128372293+DevsMarcos@users.noreply.github.com>
2024-09-11 13:09:11 -0300

    Update Professor.java

--- 2 SEMESTRE/Desenvolvimento de Aplicações/2024_DVA_TRABALHO_BIBLIOTECA/src/Class/Professor.java
+++ 2 SEMESTRE/Desenvolvimento de Aplicações/2024_DVA_TRABALHO_BIBLIOTECA/src/Class/Professor.java
@@ -1,16 +1,20 @@
 package Class;
 
-
+//Classe Professor que herda classe Membro
 public class Professor extends Membro {
+    //Atributos Específicos da Classe Professor
     private String departamento;
     private String materia;
 
+    //Construtor da classe Professor, recebe como parâmetro os atributos da Classe Membro, mais os atributos específicos da Classe
     public Professor(String nome, int idMembro, String departamento, String materia) {
         super(nome, idMembro);
         this.departamento = departamento;
         this.materia = materia;
     }
 
+    //método toStrig para realizar a representação textual, ou seja, em forma de forma de String da Classe. 
+    //Utiliza de métodos para acessar atributos da Classe Pai Membro, como nome e ID.
     @Override
     public String toString() {
         return "\n"+
