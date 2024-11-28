05f257949ab0d42f7c57f422af51d9f4aad61871
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2024-11-27 22:18:09 -0300

    Atividades aps4

---
+++ 2 SEMESTRE/Desenvolvimento de Aplicações/LISTA04_EX01/src/Moto.java
@@ -0,0 +1,41 @@
+public class Moto extends Veiculo{
+    private final double gasolina = 6.10;
+    private final double etanol = 4.10;
+    private final double diesel = 5.93;
+
+    public Moto(String placa, String combustivel, int capacidadeDoTanque) {
+        super(placa, combustivel, capacidadeDoTanque);
+    }
+
+
+    @Override
+    public double abastacer(int quantidadeLitrosAbastecida) {
+        if (quantidadeLitrosAbastecida > getCapacidadeDoTanque()){
+            System.out.println("Quatidade de abastecimento solicitada maior que o tanque do veículo placa: "+getPlaca());
+            return 0;
+        }
+        double total = calcularCusto(quantidadeLitrosAbastecida);
+        System.out.println("O valor do abastecimento do veiculo placa: "+getPlaca()+" ficou em R$: "+total);
+        return total;
+    }
+
+    @Override
+    public double calcularCusto(int quantidadeLitrosAbastecida) {
+        double precoPorLitro = 0;
+        switch (getCombustivel().toLowerCase()) {
+            case "gasolina" -> {
+                 precoPorLitro = gasolina;
+            }
+            case "etanol" -> {
+                 precoPorLitro = etanol;
+            }
+            case "disel" -> {
+                 precoPorLitro = diesel;
+            }
+            default -> {
+                System.out.println("Tipo não identificado tente novamente");
+            }
+        }
+        return precoPorLitro * quantidadeLitrosAbastecida;
+    }
+}
